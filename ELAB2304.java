ab7453e55c57d8619793d3fe3568f7b9ebbe2750
sunnioz <ChienNV.B20VT063@stu.ptit.edu.vn>
2024-04-12 11:43:56 +0700

    LiveIDE

---
+++ ELAB2304.java
@@ -0,0 +1,22 @@
+interface A{
+    void m();
+    void n(String msg);
+}
+class C implements A{
+    public void m(){
+        System.out.println("Method m() of C");
+    }
+    
+    public void n(String msg){
+        System.out.println("OOP23 " + msg);
+    }
+}
+class D extends C{boolean d=true;}
+    public class ELAB2304{
+        public static void main(String[] args) {
+            D b = new D();
+            b.m();
+            b.n("OOP23");
+            System.out.println(b.d);
+        }
+    }
